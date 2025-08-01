0445985f9bc8970047b5fc289951a80deeaf4c98
Braden Monnin <monninbm@mail.uc.edu>
2025-07-31 21:13:40 -0400

    Created member table for sql file

--- database_creation.sql
+++ database_creation.sql
@@ -9,3 +9,16 @@ CREATE TABLE Book (
     available_copies INTEGER NOT NULL CHECK (available_copies >= 0),
     CONSTRAINT check_available_copies CHECK (available_copies <= total_copies)
 );
+
+CREATE TABLE Member (
+    member_id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    phone TEXT,
+    street TEXT,
+    city TEXT,
+    state TEXT,
+    zip_code TEXT,
+    join_date DATE NOT NULL DEFAULT CURRENT_DATE
+);
